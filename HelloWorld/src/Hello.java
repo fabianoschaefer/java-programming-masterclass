82a34d0431f2fb6f251853ecdeb11b8a0bdb51a9
Fabiano Schaefer <fabiano.schaefer@compasso.com.br>
2020-06-19 00:33:23 -0300

    Variables

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Schaefer");
+
+        int myFirstNUmber = (10 + 5) + (2 * 10);
+        System.out.println("My first number is " + myFirstNUmber);
     }
 }
