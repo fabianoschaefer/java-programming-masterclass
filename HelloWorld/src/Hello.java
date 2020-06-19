6a8a5c2ba0a1983b9f50252da6daff2bc0065af5
Fabiano Schaefer <fabiano.schaefer@compasso.com.br>
2020-06-19 00:44:34 -0300

    Starting out with Expressions

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,7 +2,12 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Schaefer");
 
-        int myFirstNUmber = (10 + 5) + (2 * 10);
-        System.out.println("My first number is " + myFirstNUmber);
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
