ba80b6ac1a04749ba0bc6e0a0e10e22d1612d98a
Fabiano Schaefer <fabiano.schaefer@compasso.com.br>
2020-07-27 17:13:12 -0300

    Primitive Types Recap and the String Data Type

---
+++ Strings/src/academy/learningprogramming/Main.java
@@ -0,0 +1,24 @@
+package academy.learningprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String myString = "This is a String";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this another more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2020";
+        System.out.println("myString is equal to " + myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("LastString is equal to " + lastString);
+        double doubleNumber = 120.47d;
+        lastString = lastString + doubleNumber;
+        System.out.println("LastString is equal to " + lastString);
+    }
+}
