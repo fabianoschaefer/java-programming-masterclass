cb67c0f6e04d834ee73daa3f45a65b28b76e57be
Fabiano Schaefer <fabiano.schaefer@compasso.com.br>
2020-08-28 19:39:30 -0300

    Logical OR Operator

--- Operators/src/academy/learningprogramming/Main.java
+++ Operators/src/academy/learningprogramming/Main.java
@@ -55,8 +55,12 @@ public class Main {
             System.out.println("You got the high score!");
 
         int secondTopScore = 81;
-        if (topScore > secondTopScore && topScore < 100) {
+        if ((topScore > secondTopScore) && (topScore < 100)) {
             System.out.println("Greater than second top score and less then 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
         }
     }
 }
